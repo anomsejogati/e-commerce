11e0362af763019f8904f951ece4c74557e57e15
anomsejogati <sejogati.anom@gmail.com>
2022-10-18 14:45:23 +0700

    first commit become fullstacj MERN

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import fileUpload from "express-fileupload"
+import cors from "cors"
+import ProductRoute from "./routes/ProductRoute.js"
+
+
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+app.use(fileUpload())
+app.use(express.static("public"))
+app.use(ProductRoute)
+
+app.listen(5000, () => console.log('Server Up and Running...'))
